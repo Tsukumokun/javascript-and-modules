166fe98dc57c8309634f21738d1f917fb2fe4bc7
Tsukumo <tsukumokun@icloud.com>
2014-03-06 21:59:39 -0500

    Added main python init file
    Will probably update this later but it provides
    an entry point for the program and as well as
    project information.

---
+++ jam/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# 'jam' - is a compiler for javascript which allows for the
+#         use of modules, providing a way to import and 
+#         minify all of the source files.
+# Copyright (C) 2014 Christopher Kelley <tsukumokun(at)icloud.com>
+# 
+# This work is licensed under the 
+# Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 
+# International License. To view a copy of this license, visit 
+# http://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+__author__     = "Christopher Kelley (Tsukumo Chiaki)"
+__copyright__  = "Copyright 2014, Christopher Kelley"
+__credits__    = []
+__license__    = "Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International"
+__version__    = "0.0.1"
+__maintainer__ = "Christopher Kelley"
+__email__      = "tsukumokun@icloud.com"
+__status__     = "Development"
+
+
+# project internal imports
+from jam import JAM
+
+
+if __name__ == "__main__":
+    program = JAM()
+    program.run()
+
+
+# vim:ft=python
+
