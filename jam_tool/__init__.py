c048cf9e817af82988b474e5603aa71d3aae18b2
Tsukumo <tsukumokun@icloud.com>
2014-03-07 20:38:14 -0500

    Updated jam_tool init file
    Small modifications to init file in jam tool

--- jam_tool/__init__.py
+++ jam_tool/__init__.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 'jam' - is a compiler for javascript which allows for the
-#         use of modules, providing a way to import and 
-#         minify all of the source files.
-# Copyright (C) 2014 Christopher Kelley <tsukumokun(at)icloud.com>
-# 
-# This work is licensed under the 
-# Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 
-# International License. To view a copy of this license, visit 
-# http://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-
 """
 The jam library module file.
 """
@@ -30,7 +15,7 @@ __email__      = "tsukumokun@icloud.com"
 __status__     = "Development"
 
 
-from jam_tool.jam_tool import jam
+from jam_tool.jam import jam
 
 
 __all__ = ['jam']
