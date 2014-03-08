6184a599d2f81801cf385056c47f2f897af41c7b
Tsukumo <tsukumokun@icloud.com>
2014-03-08 14:17:08 -0500

    Added the package init file to utils
    Created a package init file in utils and made it
    import everything from its directory.

---
+++ jam_tool/utils/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+The jam library utils module file.
+"""
+
+__author__     = "Christopher Kelley (Tsukumo Chiaki)"
+__copyright__  = "Copyright 2014, Christopher Kelley"
+__credits__    = []
+__license__    = "http://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US"
+__version__    = "0.0.1"
+__maintainer__ = "Christopher Kelley"
+__email__      = "tsukumokun@icloud.com"
+__status__     = "Development"
+
+
+# project internal imports
+import jam_tool.utils.termcaps
+import jam_tool.utils.trace
+
+
+__all__ = ['termcaps','trace']
+
